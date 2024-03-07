0de4e3d969f6cf0c0c56e626c43b5cc46fce813b
paolorampino <liadserv@gmail.co>
2024-03-07 14:19:24 +0100

    up

--- Lezione0307/stringhe1.c
+++ Lezione0307/stringhe1.c
@@ -4,10 +4,11 @@ int main() {
     char stringa1[20] = {'\0'};
     char stringa2[] = "Ciao";
     printf("Scrivi esattamente 'Ciao':");
-    scanf("%s", stringa1);
+    char i ;
+    scanf("%c", &i);
     // non si possono comparare 2 array dà errore
     // if(stringa1 == stringa2) printf("%s\n", stringa1);
     // ma possiamo comparare il primo carattere
-    if(stringa1[0] == 'C' ||  stringa1[0] == 'c') printf("%s\n", stringa2);
+    if(i == 'C' ||  i == 'c') printf("%s\n", stringa2);
     return 0;
 }
