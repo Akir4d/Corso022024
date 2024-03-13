274c9e5fc498d54ca1052cd1b5b233abbe267e03
Paolo Rampino <liadserv@gmail.com>
2024-03-13 11:03:49 +0100

    Up

---
+++ Lezione0313/ifElif.py
@@ -0,0 +1,13 @@
+
+x = int (input ("Inserisci un numero: "))
+
+if x == 10:
+	print ("hai inserito 10")
+elif x > 10 and x < 21:
+	print ("hai inserito un numero maggiore di 10")
+elif x > 20 and x < 51:
+	print ("hai inserito un numero maggiore di 20")
+elif x > 50:
+	print ("hai inserito un numero maggiore di 50")
+else:
+	print ("hai inserito un numero inferiore a 10")
