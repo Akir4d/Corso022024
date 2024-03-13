ff88248ba8022d375c981929e3f7b2707c7ab3af
Paolo Rampino <liadserv@gmail.com>
2024-03-13 11:33:32 +0100

    Up

---
+++ Lezione0313/listaSemplice.py
@@ -0,0 +1,13 @@
+
+
+gioco = ["un", 2, 3, "Stella"]
+
+print (gioco[0])
+print (gioco[1])
+print (gioco[2])
+print (gioco[3])
+
+
+print ("\nFacciamo con for:")
+for gioco_di_giacomo in gioco:
+	print ("gioco_di_giacomo =",  gioco_di_giacomo)
