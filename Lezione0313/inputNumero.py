9ba4277a2938adb28852065521674365fb311022
Paolo Rampino <liadserv@gmail.com>
2024-03-13 10:20:35 +0100

    Up

---
+++ Lezione0313/inputNumero.py
@@ -0,0 +1,4 @@
+
+x = int (input ("Inserisci un numero da elevare al quadrato: "))
+
+print (x ** 2)
---
+++ Lezione0313/stringhe2.py
@@ -0,0 +1,14 @@
+
+x = input ("Inserisci una stringa:")
+
+# split consente di trasfomare la stringa in una lista si valori
+# in questo caso si e' scelto la virgola ma avremmo potuto usare pure gli spazi
+print (x.split(sep=","))
+
+# upper rende tutto maiuscolo
+print (x.upper())
+
+y = "Python e' fantastico!"
+# le stringhe si uniscono con +
+print (x + " " + y)
+
