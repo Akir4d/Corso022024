4dfcafc02f9e198d629f197ff6b515f59950857a
Paolo Rampino <liadserv@gmail.com>
2024-03-13 09:55:52 +0100

    Up

--- Lezione0313/indentazione.py
+++ Lezione0313/indentazione.py
@@ -1,9 +1,10 @@
 # Questo e' un esempio di while
 
 n = 0
-
+Nome = "Paolo"
+nome = "Pinco"
 while n < 10:
 	print (n)
-	n=n+1
+	n = n + 1
 
-print ('Ho finito')
+print ('Ho finito ' + Nome + ' ' + nome)
