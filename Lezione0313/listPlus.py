8f35ee822dcb7bf31dc17d842d3becb9629f6105
Paolo Rampino <liadserv@gmail.com>
2024-03-13 11:46:01 +0100

    Up

---
+++ Lezione0313/listPlus.py
@@ -0,0 +1,21 @@
+
+
+gioco = ["un", 2, 3, "Stella"]
+
+print (gioco)
+
+gioco[0] = "zero... un"
+
+print (gioco)
+
+gioco.append("Pronti o no ... arrivo!")
+
+print (gioco)
+
+gioco.insert(2, "Due e mezzo")
+
+print (gioco)
+
+del gioco[0]
+
+print (gioco)
