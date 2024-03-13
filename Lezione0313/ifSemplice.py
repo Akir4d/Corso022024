24187c52b11ac98baacd7fa448b3ff71842037e2
Paolo Rampino <liadserv@gmail.com>
2024-03-13 10:55:10 +0100

    Up

---
+++ Lezione0313/ifSemplice.py
@@ -0,0 +1,10 @@
+
+x  = int (input ("Inserisci un numero: "))
+
+
+if x > 10:
+	print ("il numero e' maggiore di 11")
+	print ("perche' hai inserito: " + str(x))
+else:
+	print ("il numero e' inferiore a 11")
+	print ("perche' hai inserito: " + str(x))
