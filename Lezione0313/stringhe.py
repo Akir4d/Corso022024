06b72af701b7ef96879234d301d3d160362af003
Paolo Rampino <liadserv@gmail.com>
2024-03-13 10:14:43 +0100

    Up

--- Lezione0313/stringhe.py
+++ Lezione0313/stringhe.py
@@ -1,11 +1,14 @@
 
 x = "Hello, World!"
 
+# split consente di trasfomare la stringa in una lista si valori
+# in questo caso si e' scelto la virgola ma avremmo potuto usare pure gli spazi
 print (x.split(sep=","))
 
+# upper rende tutto maiuscolo
 print (x.upper())
 
 y = "Python e' fantastico!"
-
+# le stringhe si uniscono con +
 print (x + " " + y)
 
