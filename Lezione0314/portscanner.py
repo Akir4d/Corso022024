9812d99459616f4ba376e7673e208a6899d41de1
Paolo Rampino <liadserv@gmail.com>
2024-03-14 10:58:33 +0100

    Up

--- Lezione0314/portscanner.py
+++ Lezione0314/portscanner.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 target = input("Inserisci l'indirizzo IP da scansire: ")
 portrange = input("Inserisci il port range da scansire (es 5-200): ")
@@ -7,13 +8,18 @@ lowport = int(portrange.split('-')[0])
 highport = int(portrange.split('-')[1])
 
 print("Scansisco l'host ", target, "dalla porta", lowport, "a", highport)
-
+status_p = 0
 for port in range(lowport, highport + 1):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.settimeout(1)
+	if(status_p != 0):
+			sys.stdout.write("\033[A")
+			sys.stdout.write("\033[K")
+			status_p = 0
 	status = s.connect_ex((target, port))
 	if (status == 0):
 		print('*** Porta', port, ' - APERTA ***')
 	else:
-		print('Porta', port, ' - CHIUSA')
+		print('Porta', port, ' - CHIUSA', status)
+		status_p = status
 	s.close()
